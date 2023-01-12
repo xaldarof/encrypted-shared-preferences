a3f5aa8007f5b807ce3202a932e4da64ac7c3c3a
Temur Xoldarov <temuruz2003@gmail.com>
2023-01-12 16:47:03 +0500

    fix

--- lib/enc_shared_pref.dart
+++ lib/enc_shared_pref.dart
@@ -6,7 +6,8 @@ import 'encryptor.dart';
 class EncryptedSharedPreferences {
   EncryptedSharedPreferences._();
 
-  final _key = "1212 12 1212 333 1234 256 124575";
+  String? _key;
+
   static late SharedPreferences? _sharedPreferences;
   final AESEncryptor _aes = AESEncryptor();
 
@@ -21,6 +22,10 @@ class EncryptedSharedPreferences {
     return _instance;
   }
 
+  void setEncryptionKey(String key) {
+    _key = key;
+  }
+
   Future<bool> clear() async {
     assert(_sharedPreferences != null);
     return _sharedPreferences!.clear();
@@ -28,9 +33,10 @@ class EncryptedSharedPreferences {
 
   Future<bool> setString(String dataKey, String dataValue) async {
     assert(_sharedPreferences != null);
-    Encrypted encryptedKey = _aes.encrypt(_key, dataKey);
+    assert(_key != null,"Encryption key must not be null ! To fix it use .setEncryptionKey(key) method");
+    Encrypted encryptedKey = _aes.encrypt(_key!, dataKey);
     String encryptedBase64Key = encryptedKey.base64;
-    Encrypted encryptedData = _aes.encrypt(_key, dataValue);
+    Encrypted encryptedData = _aes.encrypt(_key!, dataValue);
     String encryptedBase64Value = encryptedData.base64;
 
     return _sharedPreferences!.setString(encryptedBase64Key, encryptedBase64Value);
@@ -38,20 +44,14 @@ class EncryptedSharedPreferences {
 
   String? getString(String key) {
     assert(_sharedPreferences != null);
-    String dataKey = _aes.encrypt(_key, key).base64;
+    assert(_key != null,"Encryption key must not be null ! To fix it use .setEncryptionKey(key) method");
+    String dataKey = _aes.encrypt(_key!, key).base64;
     var value = _sharedPreferences!.getString(dataKey);
     if (value != null) {
-      var decrypted = _aes.decrypt(_key, Encrypted.fromBase64(value));
+      var decrypted = _aes.decrypt(_key!, Encrypted.fromBase64(value));
       return decrypted;
     } else {
       return null;
     }
-  }
-
-  Future<bool> remove(String key) async {
-    assert(_sharedPreferences != null);
-    String dataKey = _aes.encrypt(_key, key).base64;
-    var value = _sharedPreferences!.getString(dataKey);
-    return _sharedPreferences!.remove(value!);
   }
 }
