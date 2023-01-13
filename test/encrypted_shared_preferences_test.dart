0866d3c9cef1fb1cb6a8af8735286a44dbf65730
Temur Xoldarov <temuruz2003@gmail.com>
2023-01-13 12:05:20 +0500

    unit test

--- test/encrypted_shared_preferences_test.dart
+++ test/encrypted_shared_preferences_test.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+
+import 'package:encrypt_shared_preferences/enc_shared_pref.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  /* test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });*/
+  test('check data saved', () async {
+    final sharedPref = await EncryptedSharedPreferences.getInstance();
+    sharedPref.setEncryptionKey("xxxx xxxx xxxxxx");
+    sharedPref.setString("dataKey", "dataValue");
+    expect(sharedPref.getString('dataKey'), "dataValue");
+    var res = await sharedPref.clear();
+    expect(res, true);
+    expect(sharedPref.getString('dataKey'), null);
+  });
 }
