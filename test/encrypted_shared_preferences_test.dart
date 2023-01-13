152ad4d9143cb3fa10073f5bd611133f5549cd60
Temur Xoldarov <temuruz2003@gmail.com>
2023-01-13 12:08:48 +0500

    unit test

--- test/encrypted_shared_preferences_test.dart
+++ test/encrypted_shared_preferences_test.dart
@@ -3,14 +3,25 @@ import 'dart:math';
 import 'package:encrypt_shared_preferences/enc_shared_pref.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+void main() async {
+  final sharedPref = await EncryptedSharedPreferences.getInstance();
+  sharedPref.setEncryptionKey("xxxx xxxx xxxxxx");
+
   test('check data saved', () async {
-    final sharedPref = await EncryptedSharedPreferences.getInstance();
-    sharedPref.setEncryptionKey("xxxx xxxx xxxxxx");
     sharedPref.setString("dataKey", "dataValue");
     expect(sharedPref.getString('dataKey'), "dataValue");
+  });
+
+  test('check data clear', () async {
     var res = await sharedPref.clear();
     expect(res, true);
+    expect(sharedPref.getString('dataKey'), null);
+  });
+
+  test('check data removed', () async {
+    sharedPref.setString("dataKey", "dataValue");
+    expect(sharedPref.getString('dataKey'), "dataValue");
+    await sharedPref.remove('dataKey');
     expect(sharedPref.getString('dataKey'), null);
   });
 }
