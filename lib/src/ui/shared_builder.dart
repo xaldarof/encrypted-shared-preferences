da6186a70090af722205f5e80bd49b4924bef959
Laksh Samdariya <lakshsamdariya29@gmail.com>
2025-06-16 00:12:43 +0530

    feat : updated the builder ui for legacy pref

--- lib/src/ui/shared_builder.dart
+++ lib/src/ui/shared_builder.dart
@@ -1,29 +1,40 @@
 import 'package:encrypt_shared_preferences/provider.dart';
 import 'package:flutter/material.dart';
 
-class SharedBuilder extends StatelessWidget {
+class SharedBuilder extends StatefulWidget {
   final Set<String>? listenKeys;
-  final Widget Function(
-      EncryptedSharedPreferences preferences, String? updatedKey) builder;
-
-  final EncryptedSharedPreferences _preferences =
-      EncryptedSharedPreferences.getInstance();
+  final Widget Function(EncryptedSharedPreferences preferences, String? updatedKey) builder;
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: listenKeys != null
-          ? _preferences.observeSet(keys: listenKeys!)
-          : _preferences.observe(),
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        return builder(_preferences, snapshot.data);
-      },
-    );
-  }
+  State<SharedBuilder> createState() => _SharedBuilderState();
 
-  SharedBuilder({
+  const SharedBuilder({
     super.key,
     this.listenKeys,
     required this.builder,
   });
+}
+
+class _SharedBuilderState extends State<SharedBuilder> {
+  late EncryptedSharedPreferences _preferences;
+
+  @override
+  void initState() {
+    setInsatnce();
+    super.initState();
+  }
+
+  Future setInsatnce() async {
+    _preferences = await EncryptedSharedPreferences.create("1111111111111111");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: widget.listenKeys != null ? _preferences.observeSet(keys: widget.listenKeys!) : _preferences.observe(),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        return widget.builder(_preferences, snapshot.data);
+      },
+    );
+  }
 }
